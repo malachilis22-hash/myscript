b28e0239f58e35a747c0acfe17c1414cfa72a10a
malachilis22-hash <malachilis22@gmail.com>
2026-04-06 15:21:01 +0300

    Create myscript.lua with obfuscated Monster Hub script

---
+++ myscript/myscript.lua
@@ -0,0 +1,7 @@
+-- This is an obfuscated Monster Hub script using loadstring
+local function encryptedMonsterHub()
+    return loadstring("<your_encrypted_code_here>")()
+end
+
+-- Call the function to execute the script
+encryptedMonsterHub()
